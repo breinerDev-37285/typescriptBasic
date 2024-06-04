7ea7252d495f1ef26224689f3e33dcfeb81307ee
breiner <jose.breiner@breinerdev.com>
2024-06-04 08:32:32 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+// Primitivos
+// const a: string = "hello world";
+// let b: number;
+// const c: boolean = true;
+
+//
+
+// const d = function():void {
+
+// }
+
+// b = 5;
+
+// const e: null; // false | 0
+// const f: undefined;
+
+// Object Types
+
+// interface b {}
+// type d {}
+// class test {}
+// enum Test {
+//   UP =1
+// }
+  //Array
+  // const n: number[] = [2,5,5] 
+
+   ///Tuples
+   // type test = [string,number]
+   // const t:test = ["string", 34]
+
+
+// Special
+
+const u = 4
+
+console.log(String(u))
+Number 
+NaN
+Infinity
+
+
+// Rest
+
+// any 
+// object 
+// unknown
+// never
+
+const d: object = {}
+
+
+function error():never {
+  throw new Error('');
+}
+
+
+console.log('hello world')
