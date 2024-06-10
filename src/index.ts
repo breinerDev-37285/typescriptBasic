1a10bfcffc26cc3fd93213551e6cc3ec03729b0a
breiner <jose.breiner@breinerdev.com>
2024-06-10 07:40:30 -0500

    introducción

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,26 @@
-// Primitivos
-// const a: string = "hello world";
-// let b: number;
-// const c: boolean = true;
+import { TestNamespace } from "@base/interfaces/index";
 
-//
-
-// const d = function():void {
-
-// }
-
-// b = 5;
-
-// const e: null; // false | 0
-// const f: undefined;
-
-// Object Types
-
-// interface b {}
-// type d {}
-// class test {}
-// enum Test {
-//   UP =1
-// }
-  //Array
-  // const n: number[] = [2,5,5] 
-
-   ///Tuples
-   // type test = [string,number]
-   // const t:test = ["string", 34]
-
-
-// Special
-
-const u = 4
-
-console.log(String(u))
-Number 
-NaN
-Infinity
-
-
-// Rest
-
-// any 
-// object 
-// unknown
-// never
-
-const d: object = {}
-
-
-function error():never {
-  throw new Error('');
+const d: TestNamespace.Person ={
+  name: ''
 }
 
+// const a = new Perro1();
 
-console.log('hello world')
+// const ob = {
+//   name: 'Loki',
+//   getName: a.getName //Override
+// }
+
+// console.log(ob.getName());
+
+// const p1 = new Perro2("azul");
+// p1.eat();
+// p1.test1();
+
+// console.log({ p1 });
+
+
+// const d = Database.init();
+
+
+// console.log('asdfalksdflkasdfsd')
